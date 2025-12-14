cfc0c1464ed516acfac2fe7eec0f1b5d1c454e25
F3rits <berardhenry2022@gmail.com>
2025-12-13 20:42:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 import Game from "./game/game.js";
 
+import { walls } from "./game/walls.js";
+import { loadWalls } from "./game/walls.js";
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth - 10;
 canvas.height = window.innerHeight - 20;
 
+loadWalls(walls.length, walls[0].length);
+
 const game = new Game(ctx, canvas);
+
+const start = document.getElementById("start");
+
+start.addEventListener("click", () =>{
+start.style.visibility = "hidden";
 game.start();
+})
+
